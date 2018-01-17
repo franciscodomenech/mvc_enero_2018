1b52a0d28007d0dfdb9e1a39c8045ae3b6193fdf
vzafra <vzafra@serbatic-04678.VASS.INET>
2018-01-17 11:50:55 +0100

    Version 1.0.0 funcionando

--- src/model/questions/QuestionsAddAlumno.java
+++ src/model/questions/QuestionsAddAlumno.java
@@ -20,8 +20,8 @@ public class QuestionsAddAlumno extends Questions {
 	public void init() {
 		this.questionsText.add("Nombre:");
 		questionsText.add("Apellidos:");
-		questionsText.add("Nif");
-		questionsText.add("F. Nacimiento");
+		questionsText.add("Nif:");
+		questionsText.add("F. Nacimiento:");
 	}
 
 	@Override
